2a685df5bbdb1b94d2aafefc8e2b84b369fe5bd7
Sub <lerawgd12@gmail.com>
2026-09-24 19:54:01 +0500

    initial app

---
+++ android/app/src/main/kotlin/com/nextdevelopment/nexty/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nextdevelopment.nexty
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NextyApp extends StatelessWidget {
+  const NextyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
